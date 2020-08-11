e34001b77b170f3a47f1d5dd646c2c238a5a6911
maliccny <maliccny@gmail.com>
2020-08-11 18:15:30 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+
+@admin = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
+puts "#{User.count} users created"
+
+@post1 = Post.create!(title: 'Snapchat Meme', img_url: 'https://i.redd.it/tgulxj3ou7p41.jpg', user: @admin)
+puts "#{Post.count} posts created"
+
+@comment1 = Comment.create!(text: "cool meme man", user: @admin, post: @post1)
+puts "#{Comment.count} comments created"
+
