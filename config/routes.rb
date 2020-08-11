dbb56e2c186c5ca449c6792b468719f939d5ec06
maliccny <maliccny@gmail.com>
2020-08-10 20:15:47 -0400

    ran command to add title to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
+  resources :users do 
+    resources :posts do
+      resources :comments
+    end
+  end
+
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
+  get '/posts', to: 'posts#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
