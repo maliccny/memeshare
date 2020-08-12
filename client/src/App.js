b05f489f0a79b6a9344dd30f570f74b835df7541
maliccny <maliccny@gmail.com>
2020-08-12 11:35:20 -0400

    added app.js services for posts and comments fixed index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { loginUser, registerUser, removeToken, verifyUser } from './services/auth'
+import Main from './components/Main'
+import { withRouter } from 'react-router-dom';
+
+class App extends Component {
+  state = {
+    currentUser: null
+  }
+
+  componentDidMount() {
+    this.handleVerify();
+  }
+
+  handleLogin = async (userData) => {
+    const currentUser = await loginUser(userData);
+    this.setState({ currentUser })
+  }
+
+  handleRegister = async (userData) => {
+    const currentUser = await registerUser(userData);
+    this.setState({ currentUser })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      currentUser: null
+    })
+    localStorage.removeItem('authToken');
+    removeToken();
+    this.props.history.push('/')
+  }
+
+  handleVerify = async () => {
+    const currentUser = await verifyUser();
+    this.setState({ currentUser });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header
+          currentUser={this.state.currentUser}
+          handleLogout={this.handleLogout}
+        />
+        <Main
+          currentUser={this.state.currentUser}
+          handleLogin={this.handleLogin}
+          handleRegister={this.handleRegister}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
