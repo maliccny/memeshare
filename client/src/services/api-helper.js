7f59449157c9b99c5311566942a23217bfb9d5ac
maliccny <maliccny@gmail.com>
2020-08-14 15:38:33 -0400

    added heroku link

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'link to deployed heroku backend' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://memesharemali.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
